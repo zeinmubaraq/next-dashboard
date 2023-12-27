bde9a184ba0c36918e83a27a4c2880fa274b28ee
zeinmubaraq <zainmubaraq@gmail.com>
2023-12-27 22:18:48 +0700

    add dashboard content

--- app/page.js
+++ app/page.js
@@ -1,10 +1,83 @@
 import Admin from '@/components/Admin';
-import Image from 'next/image';
+import {
+  BuildingOffice2Icon,
+  ChartBarIcon,
+  CurrencyDollarIcon,
+  ListBulletIcon,
+  PresentationChartLineIcon,
+  ShoppingBagIcon,
+} from '@heroicons/react/24/solid';
 
 export default function Home() {
   return (
     <Admin>
-      <main>Test</main>
+      <div className="grid grid-cols-3 gap-5">
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <ChartBarIcon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">Earning</p>
+            <h3 className="text-lg font-bold">$349.5</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <PresentationChartLineIcon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">Spend This Month</p>
+            <h3 className="text-lg font-bold">$542.1</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <ShoppingBagIcon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">Sales</p>
+            <h3 className="text-lg font-bold">$311.5</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <CurrencyDollarIcon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">Your Balance</p>
+            <h3 className="text-lg font-bold">$311.5</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <ListBulletIcon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">New Tasks</p>
+            <h3 className="text-lg font-bold">231</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-4 rounded-3xl bg-white px-5 py-5 shadow-lg shadow-gray-100">
+          <div className="inline-flex h-full items-center rounded-full bg-indigo-100 px-3">
+            <BuildingOffice2Icon className="h-6 text-indigo-700" />
+          </div>
+
+          <div>
+            <p className="text-sm text-gray-400">Total Projects</p>
+            <h3 className="text-lg font-bold">$3123.5</h3>
+          </div>
+        </div>
+      </div>
     </Admin>
   );
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,38 +9,38 @@ import Link from 'next/link';
 
 export default function Sidebar() {
   return (
-    <div className="fixed z-50 min-h-full translate-x-0 bg-white shadow-2xl">
+    <div className="fixed z-50 min-h-full translate-x-0 bg-white shadow-xl">
       <div className="mx-14 mt-12 text-2xl font-bold uppercase">
         Horizon <span className="font-medium">Free</span>
       </div>
       <ul className="mt-10 space-y-1">
         <li className="border-r-4 border-indigo-600 py-2 pl-9 font-semibold">
           <Link href="#" className="flex items-center gap-x-3">
-            <HomeIcon className="h-5 w-5" />
+            <HomeIcon className="h-5 w-5 text-indigo-700" />
             Main Dashboard
           </Link>
         </li>
         <li className="flex items-center gap-x-3 py-2 pl-9 font-semibold text-gray-400">
           <Link href="#" className="flex items-center gap-x-3">
-            <ShoppingCartIcon className="h-5 w-5" />
+            <ShoppingCartIcon className="h-5 w-5 text-indigo-700" />
             NFT Markeyplace
           </Link>
         </li>
         <li className="flex items-center gap-x-3 py-2 pl-9 font-semibold text-gray-400">
           <Link href="#" className="flex items-center gap-x-3">
-            <ChartBarIcon className="h-5 w-5" />
+            <ChartBarIcon className="h-5 w-5 text-indigo-700" />
             Data Tables
           </Link>
         </li>
         <li className="flex items-center gap-x-3 py-2 pl-9 font-semibold text-gray-400">
           <Link href="#" className="flex items-center gap-x-3">
-            <UserIcon className="h-5 w-5" />
+            <UserIcon className="h-5 w-5 text-indigo-700" />
             Profile
           </Link>
         </li>
         <li className="flex items-center gap-x-3 py-2 pl-9 font-semibold text-gray-400">
           <Link href="#" className="flex items-center gap-x-3">
-            <HomeModernIcon className="h-5 w-5" />
+            <HomeModernIcon className="h-5 w-5 text-indigo-700" />
             RTL Admin
           </Link>
         </li>
